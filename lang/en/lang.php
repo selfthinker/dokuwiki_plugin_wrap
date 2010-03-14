92a10eadcc6e299016df590f10585ebb2ae34c86
Anika Henke <anika@selfthinker.org>
2010-03-14 21:13:55 +0000

    version 2010-03-14

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -5,12 +5,12 @@ $lang['picker']    = 'Wrap Plugin';
 $lang['column']    = 'column';
 $lang['box']       = 'simple centered box';
 $lang['info']      = 'info box';
-$lang['download']  = 'download box';
-$lang['help']      = 'help box';
-$lang['important'] = 'important box';
 $lang['tip']       = 'tip box';
-$lang['todo']      = 'todo box';
+$lang['important'] = 'important box';
 $lang['alert']     = 'alert box';
+$lang['help']      = 'help box';
+$lang['download']  = 'download box';
+$lang['todo']      = 'todo box';
 
 $lang['clear']     = 'clear floats';
 
