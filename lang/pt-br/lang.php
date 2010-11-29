f4ee15ba5e6abcfc5b5a8d72c930a0ec41e3fcc8
Sergio Motta <sergio@cisne.com.br>
2010-11-29 22:59:50 +0000

    added Brazilian Portuguese translation

---
+++ lang/pt-br/lang.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Brazilian portuguese language file
+ * @author     Sérgio Motta <sergio@cisne.com.br>
+ */
+
+$lang['picker']    = 'Plugin Wrap';
+
+$lang['column']    = 'coluna';
+$lang['box']       = 'caixa centralizada simples';
+$lang['info']      = 'caixa de informação';
+$lang['tip']       = 'caixa de sugestão';
+$lang['important'] = 'caixa importante';
+$lang['alert']     = 'caixa de alerta';
+$lang['help']      = 'caixa de ajuda';
+$lang['download']  = 'caixa de download';
+$lang['todo']      = 'caixa de tarefas a fazer';
+
+$lang['clear']     = 'limpar';
+
+$lang['em']        = 'especialmente enfatizado';
+$lang['hi']        = 'enfatizado';
+$lang['lo']        = 'menos significativo';
---
+++ lang/pt-br/settings.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Brazilian portuguese language file
+ * @author     Sérgio Motta <sergio@cisne.com.br>
+ */
+
+$lang['noPrefix'] = 'Quais classes (separadas por vírgula) deverão ser excluídas de receber o prefixo "wrap_"?';
+
