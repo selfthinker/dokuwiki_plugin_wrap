18a08dcabe06255af1c5abce03c0d8f582c7a7ec
Emmanuel (Laynee) <manu@seedfloyd.fr>
2011-06-04 09:20:07 +0100

    added French translation

---
+++ lang/fr/lang.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * French language file for Wrap plugin
+ * @author     Laynee <seedfloyd@gmail.com>
+ */
+
+$lang['picker']    = 'Plugin Wrap';
+
+$lang['column']    = 'colonne';
+$lang['box']       = 'bloc simple';
+$lang['info']      = 'bloc information';
+$lang['tip']       = 'bloc astuce';
+$lang['important'] = 'bloc important';
+$lang['alert']     = 'bloc alerte';
+$lang['help']      = 'bloc aide';
+$lang['download']  = 'bloc téléchargement';
+$lang['todo']      = 'bloc travaux';
+
+$lang['clear']     = 'rétablir le flux après un élément flottant';
+
+$lang['em']        = 'très important';
+$lang['hi']        = 'important';
+$lang['lo']        = 'peu important';
---
+++ lang/fr/settings.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * French language file for config of Wrap plugin
+ * @author     Laynee <seedfloyd@gmail.com>
+ */
+
+$lang['noPrefix'] = 'Quelles classes (séparées par une virgule) ne devraient pas être préfixées d\'un "wrap_" ?';
+
