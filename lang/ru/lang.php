b3d492a4c2ac4a8a77beae31f701a4c6305759ee
Aleksandr Selivanov <alexgearbox@gmail.com>
2012-08-19 11:56:25 +0100

    added Russian translation

---
+++ lang/ru/lang.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Russian language file for Wrap plugin
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Aleksandr Selivanov <alexgearbox@gmail.com>
+ */
+
+$lang['picker']    = 'Wrap';
+
+$lang['column']    = 'колонка';
+$lang['box']       = 'простой центрированный блок';
+$lang['info']      = 'блок «Информация»';
+$lang['tip']       = 'блок «Подсказка»';
+$lang['important'] = 'блок «Важно»';
+$lang['alert']     = 'блок «Тревога»';
+$lang['help']      = 'блок «Справка»';
+$lang['download']  = 'блок «Скачивание»';
+$lang['todo']      = 'блок «Список задач»';
+
+$lang['em']        = 'пометить важным';
+$lang['hi']        = 'маркер';
+$lang['lo']        = 'пометить неважным';
