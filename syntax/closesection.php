1232efa401f519e9c177018c244698bb0e301ceb
Michael Hamann <michael@content-space.de>
2012-10-11 00:31:34 +0200

    Prevent section edit buttons inside wrap content
    This prevents section edit buttons from being displayed inside the wrap
    content and makes sure that the section edit button after the wrap is
    for the section the wrap syntax is in and not for a section that is
    started inside the wrap.

---
+++ syntax/closesection.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Section close helper of the Wrap Plugin
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Michael Hamann <michael@content-space.de>
+ */
+
+if(!defined('DOKU_INC')) die();
+
+if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+require_once(DOKU_PLUGIN.'syntax.php');
+
+class syntax_plugin_wrap_closesection extends DokuWiki_Syntax_Plugin {
+
+    function getType(){ return 'formatting';}
+    function getSort(){ return 195; }
+
+    /**
+     * Dummy handler, this syntax part has no syntax but is directly added to the instructions by the div syntax
+     */
+    function handle($match, $state, $pos, &$handler){
+    }
+
+    /**
+     * Create output
+     */
+    function render($mode, &$renderer, $indata) {
+        if($mode == 'xhtml'){
+            /** @var Doku_Renderer_xhtml $renderer */
+            $renderer->finishSectionEdit();
+            return true;
+        }
+        return false;
+    }
+
+
+}
+
