55f833d2332d02b537ea94445d829fb7d6f3c51b
Marcos <katapofatico@gmail.com>
2012-10-26 22:56:02 +0100

    added Spanish translation

---
+++ lang/es/lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['picker']    = 'Wrap Plugin';
+
+$lang['column']    = 'columna';
+$lang['box']       = 'caja simple centrada';
+$lang['info']      = 'caja de información';
+$lang['tip']       = 'caja de consejo';
+$lang['important'] = 'caja importante';
+$lang['alert']     = 'caja de alerta';
+$lang['help']      = 'caja de ayuda';
+$lang['download']  = 'caja de descarga';
+$lang['todo']      = 'caja de TODO';
+
+$lang['clear']     = 'limpiar float';
+
+$lang['em']        = 'especialmente enfatizado';
+$lang['hi']        = 'destacado';
+$lang['lo']        = 'menos significativo';
---
+++ lang/es/settings.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * English language file for config of Wrap plugin
+ *
+ */
+
+$lang['noPrefix'] = '¿Qué nombres de clase (separados por comas) no deberían ser precedidos de "wrap_"?';
+
