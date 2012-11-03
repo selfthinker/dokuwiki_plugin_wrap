4a2ad1b15150152f19ebd649eaae04432d98bf47
Michael Hamann <michael@content-space.de>
2012-11-03 13:53:06 +0100

    Fix paragraph handling of the closesection instruction
    Only when the paragraph type is "block" (or "stack") and the mode is
    DOKU_LEXER_SPECIAL paragraphs around the closesection instruction are
    handled correctly.

--- syntax/closesection.php
+++ syntax/closesection.php
@@ -13,7 +13,8 @@ require_once(DOKU_PLUGIN.'syntax.php');
 
 class syntax_plugin_wrap_closesection extends DokuWiki_Syntax_Plugin {
 
-    function getType(){ return 'formatting';}
+    function getType(){ return 'substition';}
+    function getPType(){ return 'block';}
     function getSort(){ return 195; }
 
     /**
