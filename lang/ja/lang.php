de9c3209d3b5c9905c36f08160c91bdc1ea98d8e
Anika Henke <anika@selfthinker.org>
2013-03-17 19:14:22 +0000

    added Japanese language files

---
+++ lang/ja/lang.php
@@ -0,0 +1,18 @@
+<?php
+$lang['picker']    = 'Wrap プラグイン';
+
+$lang['column']    = '多段組み';
+$lang['box']       = '中央配置枠';
+$lang['info']      = '情報枠';
+$lang['tip']       = 'ヒント枠';
+$lang['important'] = '重要枠';
+$lang['alert']     = '警告枠';
+$lang['help']      = 'ヘルプ枠';
+$lang['download']  = 'ダウンロード枠';
+$lang['todo']      = 'TODO枠';
+
+$lang['clear']     = '初期化';
+
+$lang['em']        = '特に強調';
+$lang['hi']        = '強調する';
+$lang['lo']        = '強調しない';
---
+++ lang/ja/settings.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Japanese language file for config of Wrap plugin
+ *
+ */
+$lang['noPrefix'] = '"wrap_" を接頭語にした名前から除外するクラス名（カンマ区切り）';
