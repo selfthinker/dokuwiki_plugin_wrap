e32ef47c4bb3a714110edcc4da97fccbffbd07c2
Satoshi Sahara <sahara.satoshi@gmail.com>
2013-08-10 11:50:59 +0900

    Japanese language files update

--- lang/ja/settings.php
+++ lang/ja/settings.php
@@ -2,5 +2,11 @@
 /**
  * Japanese language file for config of Wrap plugin
  *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Satoshi Sahara <sahara.satoshi@gmail.com>
  */
-$lang['noPrefix'] = '"wrap_" を接頭語にした名前から除外するクラス名（カンマ区切り）';
+$lang['noPrefix'] = 'プレフィックス"wrap_" なしのCSSセレクタを例外的に適用するクラス名（カンマ区切り）';
+$lang['restrictedClasses'] = '有効性をチェックするクラス名（カンマ区切り）';
+$lang['restrictionType'] = '指定したクラスの扱い方';
+  $lang['restrictionType_o_0'] = '指定クラスを無効とし、他は有効とする';
+  $lang['restrictionType_o_1'] = '指定クラスのみを有効とする';
