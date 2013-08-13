8e9fed59720f281e3283bad5616765b8c762a0f7
Myeongjin <aranet100@gmail.com>
2013-08-13 11:56:39 +0200

    translation update

---
+++ lang/ko/settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Myeongjin <aranet100@gmail.com>
+ */
+$lang['noPrefix']              = '어느 (쉼표로 구분된) 클래스 이름은 "wrap_" 접두어에서 제외되어야 합니까?';
+$lang['restrictedClasses']     = '다음 (쉼표로 구분된) 클래스에 플러그인의 사용을 제한';
+$lang['restrictionType']       = '위의 클래스가 포함되거나 제외되어야하는 경우 지정할 제한 유형';
+$lang['restrictionType_o_0']   = '위의 클래스을 제외하고 모든 클래스 허용';
+$lang['restrictionType_o_1']   = '위의 클래스와 다른 클래스만 제한';
