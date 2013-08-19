0597f9e5ee863d3ada979ca576968eb3c929d2c6
Myeongjin <aranet100@gmail.com>
2013-08-20 01:45:17 +0200

    translation update

--- lang/ko/settings.php
+++ lang/ko/settings.php
@@ -9,4 +9,4 @@ $lang['noPrefix']              = '어느 (쉼표로 구분된) 클래스 이름�
 $lang['restrictedClasses']     = '다음 (쉼표로 구분된) 클래스에 플러그인의 사용을 제한';
 $lang['restrictionType']       = '위의 클래스가 포함되거나 제외되어야하는 경우 지정할 제한 유형';
 $lang['restrictionType_o_0']   = '위의 클래스을 제외하고 모든 클래스 허용';
-$lang['restrictionType_o_1']   = '위의 클래스와 다른 클래스만 제한';
+$lang['restrictionType_o_1']   = '위의 클래스 외의 다른 클래스만 제한';
