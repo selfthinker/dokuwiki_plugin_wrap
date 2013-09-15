2b4243600aaa2dff6f00401ec66a2dbc2d262431
schplurtz <Schplurtz@laposte.net>
2013-09-15 16:30:25 +0200

    translation update

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -1,23 +1,22 @@
 <?php
+
 /**
- * French language file for Wrap plugin
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
  * @author     Laynee <seedfloyd@gmail.com>
+ * @author schplurtz <Schplurtz@laposte.net>
  */
-
-$lang['picker']    = 'Plugin Wrap';
-
-$lang['column']    = 'colonne';
-$lang['box']       = 'bloc simple';
-$lang['info']      = 'bloc information';
-$lang['tip']       = 'bloc astuce';
-$lang['important'] = 'bloc important';
-$lang['alert']     = 'bloc alerte';
-$lang['help']      = 'bloc aide';
-$lang['download']  = 'bloc téléchargement';
-$lang['todo']      = 'bloc travaux';
-
-$lang['clear']     = 'rétablir le flux après un élément flottant';
-
-$lang['em']        = 'très important';
-$lang['hi']        = 'important';
-$lang['lo']        = 'peu important';
+$lang['picker']                = 'Plugin Wrap';
+$lang['column']                = 'colonne';
+$lang['box']                   = 'bloc simple';
+$lang['info']                  = 'bloc information';
+$lang['tip']                   = 'bloc astuce';
+$lang['important']             = 'bloc important';
+$lang['alert']                 = 'bloc alerte';
+$lang['help']                  = 'bloc aide';
+$lang['download']              = 'bloc téléchargement';
+$lang['todo']                  = 'bloc travaux';
+$lang['clear']                 = 'rétablir le flux après un élément flottant';
+$lang['em']                    = 'particulièrement important';
+$lang['hi']                    = 'important';
+$lang['lo']                    = 'peu important';
--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -1,8 +1,13 @@
 <?php
+
 /**
- * French language file for config of Wrap plugin
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
  * @author     Laynee <seedfloyd@gmail.com>
+ * @author schplurtz <Schplurtz@laposte.net>
  */
-
-$lang['noPrefix'] = 'Quelles classes (séparées par une virgule) ne devraient pas être préfixées d\'un "wrap_" ?';
-
+$lang['noPrefix']              = 'Quelles classes (séparées par une virgule) ne devraient pas être préfixées d\'un "wrap_" ?';
+$lang['restrictedClasses']     = 'Restreindre l\'utilisation de cette extension à ces classes. (liste séparée par des virgules)';
+$lang['restrictionType']       = 'Type de restriction. Indique s\'il faut inclure ou exclure les classes ci-dessus.';
+$lang['restrictionType_o_0']   = 'Autoriser toutes les classes sauf celles ci-dessus.';
+$lang['restrictionType_o_1']   = 'N\'autoriser que les classes ci dessus.';
