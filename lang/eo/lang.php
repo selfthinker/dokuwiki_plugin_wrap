99fcd171806326810e2bd35ecd22fe861bee9723
Robert Bogenschneider <bogi@uea.org>
2013-10-29 21:25:05 +0100

    translation update

---
+++ lang/eo/lang.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Robert Bogenschneider <bogi@uea.org>
+ */
+$lang['picker']                = 'Kromaĵo Wrap';
+$lang['column']                = 'kolumno';
+$lang['box']                   = 'simpla centrita skatolo';
+$lang['info']                  = 'inform-skatolo';
+$lang['tip']                   = 'konsil-skatolo';
+$lang['important']             = 'grava skatolo';
+$lang['alert']                 = 'avert-skatolo';
+$lang['help']                  = 'help-skatolo';
+$lang['download']              = 'elŝut-skatolo';
+$lang['todo']                  = 'farendaĵ-skatolo';
+$lang['clear']                 = 'liberigi la fluon';
+$lang['em']                    = 'aparte emfazite';
+$lang['hi']                    = 'markite';
+$lang['lo']                    = 'malpli grave';
---
+++ lang/eo/settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Robert Bogenschneider <bogi@uea.org>
+ */
+$lang['noPrefix']              = 'Kiujn klasnomojn (komo-disigite) ekskludi de la prefikso "wrap_"?';
+$lang['restrictedClasses']     = 'limigi uzon de la kromaĵo al tiuj klasoj (komo-disigite)';
+$lang['restrictionType']       = 'tipo de limigo, klarigas, ĉu tiuj klasoj estu en- aŭ ekskludataj';
+$lang['restrictionType_o_0']   = 'permesi ĉiujn klasojn krom tiuj';
+$lang['restrictionType_o_1']   = 'limigi nur al tiuj klasoj, neniuj aliaj';
