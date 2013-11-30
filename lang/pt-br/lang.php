27be5ac10c9c4ccb8a2fca82033be60c5c880fff
Juliano Marconi Lanigra <juliano.marconi@gmail.com>
2013-11-30 23:15:06 +0100

    translation update

--- lang/pt-br/lang.php
+++ lang/pt-br/lang.php
@@ -1,23 +1,21 @@
 <?php
+
 /**
- * Brazilian portuguese language file
- * @author     Sérgio Motta <sergio@cisne.com.br>
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Sérgio Motta <sergio@cisne.com.br>
  */
-
-$lang['picker']    = 'Plugin Wrap';
-
-$lang['column']    = 'coluna';
-$lang['box']       = 'caixa centralizada simples';
-$lang['info']      = 'caixa de informação';
-$lang['tip']       = 'caixa de sugestão';
-$lang['important'] = 'caixa importante';
-$lang['alert']     = 'caixa de alerta';
-$lang['help']      = 'caixa de ajuda';
-$lang['download']  = 'caixa de download';
-$lang['todo']      = 'caixa de tarefas a fazer';
-
-$lang['clear']     = 'limpar';
-
-$lang['em']        = 'especialmente enfatizado';
-$lang['hi']        = 'enfatizado';
-$lang['lo']        = 'menos significativo';
+$lang['picker']                = 'Plugin Wrap';
+$lang['column']                = 'coluna';
+$lang['box']                   = 'caixa centralizada simples';
+$lang['info']                  = 'caixa de informação';
+$lang['tip']                   = 'caixa de sugestão';
+$lang['important']             = 'caixa importante';
+$lang['alert']                 = 'caixa de alerta';
+$lang['help']                  = 'caixa de ajuda';
+$lang['download']              = 'caixa de download';
+$lang['todo']                  = 'caixa de tarefas a fazer';
+$lang['clear']                 = 'limpar';
+$lang['em']                    = 'especialmente enfatizado';
+$lang['hi']                    = 'enfatizado';
+$lang['lo']                    = 'menos significativo';
--- lang/pt-br/settings.php
+++ lang/pt-br/settings.php
@@ -1,8 +1,13 @@
 <?php
+
 /**
- * Brazilian portuguese language file
- * @author     Sérgio Motta <sergio@cisne.com.br>
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Sérgio Motta <sergio@cisne.com.br>
+ * @author Juliano Marconi Lanigra <juliano.marconi@gmail.com>
  */
-
-$lang['noPrefix'] = 'Quais classes (separadas por vírgula) deverão ser excluídas de receber o prefixo "wrap_"?';
-
+$lang['noPrefix']              = 'Quais classes (separadas por vírgula) deverão ser excluídas de receber o prefixo "wrap_"?';
+$lang['restrictedClasses']     = 'uso restrito do plugin para essas classes (separadas por vírgula)';
+$lang['restrictionType']       = 'tipo de restrição, especifica se as classes acima deveriam ser incluídas ou excluídas';
+$lang['restrictionType_o_0']   = 'permite todas as classes exceto as acima';
+$lang['restrictionType_o_1']   = 'restrita somente às classes acima e nenhuma outra';
