89c3117de214f90f715d44ef70fc7362829d56fb
Nicolas Friedli <nicolas@theologique.ch>
2013-12-02 15:10:06 +0100

    translation update

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -1,23 +1,22 @@
 <?php
+
 /**
- * French language file for Wrap plugin
- * @author     Laynee <seedfloyd@gmail.com>
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Laynee <seedfloyd@gmail.com>
+ * @author Nicolas Friedli <nicolas@theologique.ch>
  */
-
-$lang['picker']    = 'Plugin Wrap';
-
-$lang['column']    = 'colonne';
-$lang['box']       = 'bloc simple';
-$lang['info']      = 'bloc information';
-$lang['tip']       = 'bloc astuce';
-$lang['important'] = 'bloc important';
-$lang['alert']     = 'bloc alerte';
-$lang['help']      = 'bloc aide';
-$lang['download']  = 'bloc téléchargement';
-$lang['todo']      = 'bloc travaux';
-
-$lang['clear']     = 'rétablir le flux après un élément flottant';
-
-$lang['em']        = 'très important';
-$lang['hi']        = 'important';
-$lang['lo']        = 'peu important';
+$lang['picker']                = 'Plugin Wrap';
+$lang['column']                = 'colonne';
+$lang['box']                   = 'bloc simple centré';
+$lang['info']                  = 'bloc information';
+$lang['tip']                   = 'bloc astuce';
+$lang['important']             = 'bloc important';
+$lang['alert']                 = 'bloc alerte';
+$lang['help']                  = 'bloc aide';
+$lang['download']              = 'bloc téléchargement';
+$lang['todo']                  = 'bloc travaux';
+$lang['clear']                 = 'rétablir le flux après un élément flottant';
+$lang['em']                    = 'très important';
+$lang['hi']                    = 'important';
+$lang['lo']                    = 'peu important';
