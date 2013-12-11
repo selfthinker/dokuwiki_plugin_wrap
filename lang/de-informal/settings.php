0cc31cc0d019a86f3b8456141895bd5a1522525c
Anika Henke <anika@selfthinker.org>
2013-12-11 10:45:40 +0000

    added de-informal language

---
+++ lang/de-informal/settings.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * German language file for config of Wrap plugin
+ *
+ */
+
+$lang['noPrefix'] = 'Welche (mit Komma getrennten) Klassen sollen nicht mit "wrap_" anfangen?';
+$lang['restrictedClasses'] = 'Beschränke die Benutzung des Plugins auf diese (mit Komma getrennten) Klassen';
+$lang['restrictionType'] = 'Beschränkungsart, setzt fest ob obige Klassen ein- oder ausgeschlossen werden sollen';
+  $lang['restrictionType_o_0'] = 'Erlaube alle Klassen außer die obigen';
+  $lang['restrictionType_o_1'] = 'Beschränke auf die obigen Klassen und erlaube keine anderen';
