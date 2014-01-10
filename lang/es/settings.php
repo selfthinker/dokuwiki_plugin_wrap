cb54e6bafd049b1b299360cfaa3ca3f6e72670ce
Juan Asensio Sánchez <okelet@gmail.com>
2014-01-10 09:30:13 +0100

    translation update

--- lang/es/settings.php
+++ lang/es/settings.php
@@ -1,8 +1,12 @@
 <?php
+
 /**
- * English language file for config of Wrap plugin
- *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Juan Asensio Sánchez <okelet@gmail.com>
  */
-
-$lang['noPrefix'] = '¿Qué nombres de clase (separados por comas) no deberían ser precedidos de "wrap_"?';
-
+$lang['noPrefix']              = '¿Qué nombres de clase (separados por comas) no deberían ser precedidos de "wrap_"?';
+$lang['restrictedClasses']     = 'restringir el uso de este plugin a estas clases (separadas por comas)';
+$lang['restrictionType']       = 'tipo de restricción, especifica si las clases anteriores serán incluidas o ecluidas';
+$lang['restrictionType_o_0']   = 'permitir todas las clases excepto las anteriores';
+$lang['restrictionType_o_1']   = 'restringir a sólo las clases anteriores y no otras';
