71f6b83f621b0602f283631405214cfba0267605
Edmondo <snarchio@gmail.com>
2014-01-22 14:00:09 +0100

    translation update

---
+++ lang/it/lang.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Edmondo <snarchio@gmail.com>
+ */
+$lang['column']                = 'colonna';
+$lang['em']                    = 'enfatizzato speciale';
+$lang['hi']                    = 'evidenziato';
+$lang['lo']                    = 'meno importante';
