248b626d6ac02bf267ad4603cf26a218ae218ce4
Ilya Rozhkov <impeck@ya.ru>
2014-02-01 13:00:32 +0100

    translation update

--- lang/ru/lang.php
+++ lang/ru/lang.php
@@ -1,23 +1,22 @@
 <?php
+
 /**
- * Russian language file for Wrap plugin
- *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Aleksandr Selivanov <alexgearbox@gmail.com>
+ * 
+ * @author Aleksandr Selivanov <alexgearbox@gmail.com>
+ * @author Ilya Rozhkov <impeck@ya.ru>
  */
-
-$lang['picker']    = 'Wrap';
-
-$lang['column']    = 'колонка';
-$lang['box']       = 'простой центрированный блок';
-$lang['info']      = 'блок «Информация»';
-$lang['tip']       = 'блок «Подсказка»';
-$lang['important'] = 'блок «Важно»';
-$lang['alert']     = 'блок «Тревога»';
-$lang['help']      = 'блок «Справка»';
-$lang['download']  = 'блок «Скачивание»';
-$lang['todo']      = 'блок «Список задач»';
-
-$lang['em']        = 'пометить важным';
-$lang['hi']        = 'маркер';
-$lang['lo']        = 'пометить неважным';
+$lang['picker']                = 'Wrap';
+$lang['column']                = 'колонка';
+$lang['box']                   = 'простой центрированный блок';
+$lang['info']                  = 'блок «Информация»';
+$lang['tip']                   = 'блок «Подсказка»';
+$lang['important']             = 'блок «Важно»';
+$lang['alert']                 = 'блок «Тревога»';
+$lang['help']                  = 'блок «Справка»';
+$lang['download']              = 'блок «Скачивание»';
+$lang['todo']                  = 'блок «Список задач»';
+$lang['clear']                 = 'очистить float\'ы';
+$lang['em']                    = 'пометить важным';
+$lang['hi']                    = 'маркер';
+$lang['lo']                    = 'пометить неважным';
---
+++ lang/ru/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Ilya Rozhkov <impeck@ya.ru>
+ */
+$lang['restrictedClasses']     = 'классы плагина, которые нельзя использовать (перечислите через запятую)';
+$lang['syntaxDiv']             = 'Какой синтаксис использовать для создания блоков и примечаний?';
+$lang['syntaxSpan']            = 'Какой синтаксис использовать для создания блоков выделения внутри текста?';
