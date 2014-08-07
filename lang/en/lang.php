f5323c89aea682882a1522c86d33adc0d42affe3
Allan Nott <hostsour@gmail.com>
2014-08-07 11:10:06 +0200

    translation update

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,19 +1,20 @@
 <?php
 
-$lang['picker']    = 'Wrap Plugin';
-
-$lang['column']    = 'columns';
-$lang['box']       = 'simple centered box';
-$lang['info']      = 'info box';
-$lang['tip']       = 'tip box';
-$lang['important'] = 'important box';
-$lang['alert']     = 'alert box';
-$lang['help']      = 'help box';
-$lang['download']  = 'download box';
-$lang['todo']      = 'todo box';
-
-$lang['clear']     = 'clear floats';
-
-$lang['em']        = 'especially emphasised';
-$lang['hi']        = 'highlighted';
-$lang['lo']        = 'less significant';
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ */
+$lang['picker']                = 'Wrap Plugin';
+$lang['column']                = 'columns';
+$lang['box']                   = 'simple centered box';
+$lang['info']                  = 'info box';
+$lang['tip']                   = 'tip box';
+$lang['important']             = 'important box';
+$lang['alert']                 = 'alert box';
+$lang['help']                  = 'help box';
+$lang['download']              = 'download box';
+$lang['todo']                  = 'todo box';
+$lang['clear']                 = 'clear floats';
+$lang['em']                    = 'especially emphasised';
+$lang['hi']                    = 'highlighted';
+$lang['lo']                    = 'less significant';
--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,13 +1,13 @@
 <?php
-/**
- * English language file for config of Wrap plugin
- *
- */
 
-$lang['noPrefix'] = 'Which (comma separated) class names should be excluded from being prefixed with "wrap_"?';
-$lang['restrictedClasses'] = 'restrict usage of plugin to these (comma separated) classes';
-$lang['restrictionType'] = 'restriction type, specifies if classes above shall be included or excluded';
-  $lang['restrictionType_o_0'] = 'allow all classes except the ones above';
-  $lang['restrictionType_o_1'] = 'restrict to only the classes above and no others';
-$lang['syntaxDiv'] = 'Which syntax should be used in the toolbar picker for block wraps?';
-$lang['syntaxSpan'] = 'Which syntax should be used in the toolbar picker for inline wraps?';
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ */
+$lang['noPrefix']              = 'Which (comma separated) class names should be excluded from being prefixed with "wrap_"?';
+$lang['restrictedClasses']     = 'restrict usage of plugin to these (comma separated) classes';
+$lang['restrictionType']       = 'restriction type, specifies if classes above shall be included or excluded';
+$lang['restrictionType_o_0']   = 'allow all classes except the ones above';
+$lang['restrictionType_o_1']   = 'restrict to only the classes above and no others';
+$lang['syntaxDiv']             = 'Which syntax should be used in the toolbar picker for block wraps?';
+$lang['syntaxSpan']            = 'Which syntax should be used in the toolbar picker for inline wraps?';
