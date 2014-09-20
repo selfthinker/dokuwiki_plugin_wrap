25bb55b8097011cc93ac0dc790dfbb26832c4287
Jaroslav Lichtblau <jlichtblau@seznam.cz>
2014-09-20 11:30:05 +0200

    translation update

---
+++ lang/cs/lang.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Jaroslav Lichtblau <jlichtblau@seznam.cz>
+ */
+$lang['column']                = 'sloupce';
+$lang['box']                   = 'jednoduchý vystředěný rámeček';
+$lang['info']                  = 'informační rámeček';
+$lang['tip']                   = 'rámeček s radou';
+$lang['important']             = 'důležitý rámeček';
+$lang['alert']                 = 'výstražný rámeček';
+$lang['help']                  = 'pomocný rámeček';
+$lang['download']              = 'rámeček s odkazem ke stažení';
+$lang['todo']                  = 'rámeček úkolu';
+$lang['em']                    = 'zvláštně zdůrazněné';
+$lang['hi']                    = 'zvýrazněné';
+$lang['lo']                    = 'méně důležité';
