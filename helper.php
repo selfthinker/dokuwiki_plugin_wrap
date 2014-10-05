a20a0e44a812b02ef53c65a1fda562180f91f0aa
LarsDW223 <lars_paulsen@web.de>
2014-10-05 14:28:38 +0200

    Implemented partial support for ODT export: + Container:   + Supported:     + Colors match CSS     + Resize in height if content gets to big     + Picture is present in ODT also     - Picture is not vertically centered     - Picture is re-sized if the box is re-sized e.g. in OpenOffice Writer + Spans:   + Supported:     + Colors match CSS     + Styles match CSS (e.g. subscript, emphasis)     - Small picture is missing     - Alignment and width are ignored - The rest is not supported right now

--- helper.php
+++ helper.php
@@ -10,6 +10,102 @@
 if(!defined('DOKU_INC')) die();
 
 class helper_plugin_wrap extends DokuWiki_Plugin {
+    public $known_odt_styles = array ("box", "info", "important", "alert", "tip", "help", "todo", "download",
+                                      "danger", "warning", "caution", "notice", "safety", "hi", "lo", "em", "column");
+
+    // For each style list the background color and the name of the corresponding image, if any.
+    // Color values like #eee do not work in odt so they need to be expanded to #e0e0e0.
+    public $odt_styles = array("default"   => array ("type"     => "mark",
+                                                     "fo_color" => "#000000",
+                                                     "bg_color" => "#ffffff",
+                                                     "style"    => NULL,
+                                                     "picture" => NULL ),
+                               "box"       => array ("type"     => "container",
+                                                     "fo_color" => "#000000",
+                                                     "bg_color" => "#eeeeee",
+                                                     "style"    => NULL,
+                                                     "picture" => NULL ),
+                               "info"      => array ("type"     => "container",
+                                                     "fo_color" => "#000000",
+                                                     "bg_color" => "#d1d7df",
+                                                     "style"    => NULL,
+                                                     "picture" => "info.png" ),
+                               "important" => array ("type"     => "container",
+                                                     "fo_color" => "#000000",
+                                                     "bg_color" => "#ffd39f",
+                                                     "style"    => NULL,
+                                                     "picture" => "important.png" ),
+                               "alert"     => array ("type"     => "container",
+                                                     "fo_color" => "#000000",
+                                                     "bg_color" => "#ffbcaf",
+                                                     "style"    => NULL,
+                                                     "picture" => "alert.png" ),
+                               "tip"       => array ("type"     => "container",
+                                                     "fo_color" => "#000000",
+                                                     "bg_color" => "#fff79f",
+                                                     "style"    => NULL,
+                                                     "picture" => "tip.png" ),
+                               "help"      => array ("type"     => "container",
+                                                     "fo_color" => "#000000",
+                                                     "bg_color" => "#dcc2ef",
+                                                     "style"    => NULL,
+                                                     "picture" => "help.png" ),
+                               "todo"      => array ("type"     => "container",
+                                                     "fo_color" => "#000000",
+                                                     "bg_color" => "#c2efdd",
+                                                     "style"    => NULL,
+                                                     "picture" => "todo.png" ),
+                               "download"  => array ("type"     => "container",
+                                                     "fo_color" => "#000000",
+                                                     "bg_color" => "#c2efdd",
+                                                     "style"    => NULL,
+                                                     "picture" => "download.png" ),
+                               "danger"    => array ("type"     => "container",
+                                                     "fo_color" => "#ffffff",
+                                                     "bg_color" => "#cc0000",
+                                                     "style"    => NULL,
+                                                     "picture" => NULL ),
+                               "warning"   => array ("type"     => "container",
+                                                     "fo_color" => "#000000",
+                                                     "bg_color" => "#ff6600",
+                                                     "style"    => NULL,
+                                                     "picture" => NULL ),
+                               "caution"   => array ("type"     => "container",
+                                                     "fo_color" => "#000000",
+                                                     "bg_color" => "#ffff00",
+                                                     "style"    => NULL,
+                                                     "picture" => NULL ),
+                               "notice"    => array ("type"     => "container",
+                                                     "fo_color" => "#ffffff",
+                                                     "bg_color" => "#0066ff",
+                                                     "style"    => NULL,
+                                                     "picture" => NULL ),
+                               "safety"    => array ("type"     => "container",
+                                                     "fo_color" => "#ffffff",
+                                                     "bg_color" => "#009900",
+                                                     "style"    => NULL,
+                                                     "picture" => NULL ),
+                               "hi"        => array ("type"     => "mark",
+                                                     "fo_color" => "#000000",
+                                                     "bg_color" => "#ffff99",
+                                                     "style"    => NULL,
+                                                     "picture" => NULL ),
+                               "lo"        => array ("type"     => "mark",
+                                                     "fo_color" => "#666666",
+                                                     "bg_color" => "#ffffff",
+                                                     "style"    => "subscript",
+                                                     "picture" => NULL ),
+                               "em"        => array ("type"     => "mark",
+                                                     "fo_color" => "#cc0000",
+                                                     "bg_color" => "#ffffff",
+                                                     "style"    => "strong",
+                                                     "picture" => NULL ),
+                               "column"    => array ("type"     => "column",
+                                                     "fo_color" => "#000000",
+                                                     "bg_color" => "#ffffff",
+                                                     "style"    => NULL,
+                                                     "picture" => NULL ),
+                               );
 
     /**
      * get attributes (pull apart the string between '<wrap' and '>')
@@ -65,6 +161,13 @@ class helper_plugin_wrap extends DokuWiki_Plugin {
             }
             $prefix = in_array($token, $noPrefix) ? '' : 'wrap_';
             $attr['class'] = (isset($attr['class']) ? $attr['class'].' ' : '').$prefix.$token;
+
+            // get odt style
+            foreach ($this->known_odt_styles as $style) {
+                if ( $style == $token ) {
+                    $attr['odt_style'] = $token;
+                }
+            }
         }
 
         //get dir
@@ -103,6 +206,43 @@ class helper_plugin_wrap extends DokuWiki_Plugin {
             }
             // only write lang if it's a language in lang2dir.conf
             if($attr['dir'])   $out .= ' lang="'.$attr['lang'].'" xml:lang="'.$attr['lang'].'" dir="'.$attr['dir'].'"';
+        }
+        if ($mode=='odt') {
+            if($attr['class']) $out .= ' class="'.hsc($attr['class']).' '.$addClass.'"';
+            // if used in other plugins, they might want to add their own class(es)
+            elseif($addClass)  $out .= ' class="'.$addClass.'"';
+            if($attr['id'])    $out .= ' id="'.hsc($attr['id']).'"';
+            // width on spans normally doesn't make much sense, but in the case of floating elements it could be used
+            if($attr['width']) {
+                if (strpos($attr['width'],'%') !== false) {
+                    $out .= ' width="'.hsc($attr['width']).'"';
+                } else {
+                    // anything but % should be 100% when the screen gets smaller
+                    $out .= ' width=" '.hsc($attr['width']).'"'; //; max-width: 100%;"';
+                }
+            }
+            // only write lang if it's a language in lang2dir.conf
+            if($attr['dir'])   $out .= ' lang="'.$attr['lang'].'" xml:lang="'.$attr['lang'].'" dir="'.$attr['dir'].'"';
+            // if the style is known get it's properties otherwise use default style properties
+            if($attr['odt_style']) {
+                $out .= ' odt_style="'.$attr['odt_style'].'"';
+                $out .= ' odt_type="'.$this->odt_styles[$attr['odt_style']]['type'].'"';
+                $out .= ' odt_bg="'.$this->odt_styles[$attr['odt_style']]['bg_color'].'"';
+                $out .= ' odt_fo="'.$this->odt_styles[$attr['odt_style']]['fo_color'].'"';
+                if ( $this->odt_styles[$attr['odt_style']]['style'] != NULL )
+                    $out .= ' odt_fo_style="'.$this->odt_styles[$attr['odt_style']]['style'].'"';
+                if ( $this->odt_styles[$attr['odt_style']]['picture'] != NULL )
+                    $out .= ' odt_pic="'.$this->odt_styles[$attr['odt_style']]['picture'].'"';
+            } else {
+                $out .= ' odt_style="default"';
+                $out .= ' odt_type="'.$this->odt_styles['default']['type'].'"';
+                $out .= ' odt_bg="'.$this->odt_styles['default']['bg_color'].'"';
+                $out .= ' odt_fo="'.$this->odt_styles['default']['fo_color'].'"';
+                if ( $this->odt_styles['default']['style'] != NULL )
+                    $out .= ' odt_fo_style="'.$this->odt_styles['default']['style'].'"';
+                if ( $this->odt_styles['default']['picture'] != NULL )
+                    $out .= ' odt_pic="'.$this->odt_styles['default']['picture'].'"';
+            }
         }
 
         return $out;
--- syntax/span.php
+++ syntax/span.php
@@ -15,6 +15,7 @@ class syntax_plugin_wrap_span extends DokuWiki_Syntax_Plugin {
 
     protected $entry_pattern = '<span.*?>(?=.*?</span>)';
     protected $exit_pattern  = '</span>';
+    protected $odt_fo_style;
 
     function getType(){ return 'formatting';}
     function getAllowedTypes() { return array('formatting', 'substition', 'disabled'); }
@@ -76,6 +77,91 @@ class syntax_plugin_wrap_span extends DokuWiki_Syntax_Plugin {
 
                 case DOKU_LEXER_EXIT:
                     $renderer->doc .= "</span>";
+                    break;
+            }
+            return true;
+        }
+        if($mode == 'odt'){
+            switch ($state) {
+                case DOKU_LEXER_ENTER:
+                    // Get attributes.
+                    $wrap =& plugin_load('helper', 'wrap');
+                    $attr = $wrap->buildAttributes($data, 'plugin_wrap',$mode);
+
+                    // Parse attributes.
+                    $token = explode(' ', $attr);
+                    $odt_style='NOT FOUND!!!';
+                    $odt_bg='#FFFFFF';
+                    $odt_fo='#000000';
+                    $class=NULL;
+                    $this->odt_fo_style=NULL;
+                    foreach ($token as $i => $value) {
+                        if ( substr($value,0,6) == 'class=' )
+                        {
+                            $class = trim(substr($value,7), '"');
+                            continue;
+                        }
+                        if ( substr($value,0,10) == 'odt_style=' )
+                        {
+                            $odt_style = trim(substr($value,10), '"');
+                            continue;
+                        }
+                        if ( substr($value,0,7) == 'odt_bg=' )
+                        {
+                            $odt_bg = trim(substr($value,8), '"');
+                            continue;
+                        }
+                        if ( substr($value,0,7) == 'odt_fo=' )
+                        {
+                            $odt_fo = trim(substr($value,8), '"');
+                            continue;
+                        }
+                        if ( substr($value,0,12) == 'odt_fo_style' )
+                        {
+                            $this->odt_fo_style = trim(substr($value,13), '"');
+                            continue;
+                        }
+                    }                
+
+                    // Add our styles.
+                    $style_name='pluginwrap_SPAN'.$odt_style.$margin_left.trim($odt_bg,'#').trim($odt_fo,'#');
+                    $renderer->autostyles[$style_name] =
+                    '<style:style style:name="'.$style_name.'_text_box" style:family="text">
+                         <style:text-properties fo:color="'.$odt_fo.'" fo:background-color="'.$odt_bg.'"/>
+                         <style:paragraph-properties
+                          fo:margin-left="'.$margin_left.'pt" fo:margin-right="0pt" fo:text-indent="0cm"/>
+                     </style:style>';
+
+                    // FIXME: I did not find a way to place a small image between the text and
+                    //        letting the text flow normal around it as if like the picture is just a letter.
+                    // So we not use an image and use a simple text span to color the text with the wanted
+                    // font and background color.
+                    $renderer->doc .= '<text:span text:style-name="'.$style_name.'_text_box">';
+
+                    // If a font style was found then set a nested span.
+                    // FIXME: allow a list of font styles e.g. 'odt_fo_style="strong underline"'
+                    if ( $this->odt_fo_style != NULL ) {
+                        if ( $this->odt_fo_style == 'strong' ) {
+                            $renderer->strong_open ();
+                        }
+                        if ( $this->odt_fo_style == 'subscript' ) {
+                            $renderer->subscript_open ();
+                        }
+                    }
+                    break;
+
+                case DOKU_LEXER_EXIT:
+                    // If a font style was found then close the nested span.
+                    if ( $this->odt_fo_style != NULL ) {
+                        if ( $this->odt_fo_style == 'strong' ) {
+                            $renderer->strong_close ();
+                        }
+                        if ( $this->odt_fo_style == 'subscript' ) {
+                            $renderer->subscript_close ();
+                        }
+                    }
+
+                    $renderer->doc .= '</text:span>';
                     break;
             }
             return true;
