29bb0078dd82a2eb460b5f816d6a6d09ac92bd5d
Rouslan <root.of.system@gmail.com>
2014-10-31 14:55:06 +0100

    translation update

--- lang/ru/settings.php
+++ lang/ru/settings.php
@@ -2,11 +2,14 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- *
+ * 
  * @author Ilya Rozhkov <impeck@ya.ru>
  * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
+ * @author Rouslan <root.of.system@gmail.com>
  */
+$lang['noPrefix']              = 'К каким (разделенным запятыми) именам классов не должен быть приписан префикс "wrap_" ?';
 $lang['restrictedClasses']     = 'Классы плагина, которые нельзя использовать (перечислите через запятую)';
+$lang['restrictionType']       = 'Тип ограничения, указывающий, должны ли быть включены или исключены классы выше';
 $lang['restrictionType_o_0']   = 'разрешить все, за исключением классов, указанных выше';
 $lang['restrictionType_o_1']   = 'ограничить только классами, указанными выше';
 $lang['syntaxDiv']             = 'Какой синтаксис использовать для создания блоков и примечаний?';
