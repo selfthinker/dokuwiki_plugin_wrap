3659f506137565956ef2d8f8d04ad4af29537f6b
Thymopat <thymopat@gmail.com>
2014-11-13 11:20:20 +0100

    translation update

--- lang/de-informal/lang.php
+++ lang/de-informal/lang.php
@@ -1,19 +1,21 @@
 <?php
 
-$lang['picker']    = 'Wrap-Plugin';
-
-$lang['column']    = 'Spalten';
-$lang['box']       = 'einfache zentrierte Box';
-$lang['info']      = 'Info-Box';
-$lang['tip']       = 'Tip-Box';
-$lang['important'] = 'Wichtig-Box';
-$lang['alert']     = 'Warn-Box';
-$lang['help']      = 'Hilfe-Box';
-$lang['download']  = 'Download-Box';
-$lang['todo']      = 'Zu-Erledigen-Box';
-
-$lang['clear']     = 'Floats beseitigen';
-
-$lang['em']        = 'wichtig';
-$lang['hi']        = 'markieren';
-$lang['lo']        = 'weniger wichtig';
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Thymopat <thymopat@gmail.com>
+ */
+$lang['picker']                = 'Wrap-Plugin';
+$lang['column']                = 'Spalten';
+$lang['box']                   = 'einfach zentrierte Box';
+$lang['info']                  = 'Info-Box';
+$lang['tip']                   = 'Tip-Box';
+$lang['important']             = 'Wichtig-Box';
+$lang['alert']                 = 'Warn-Box';
+$lang['help']                  = 'Hilfe-Box';
+$lang['download']              = 'Download-Box';
+$lang['todo']                  = 'Zu-Erledigen-Box';
+$lang['clear']                 = 'Floats beseitigen';
+$lang['em']                    = 'wichtig';
+$lang['hi']                    = 'markieren';
+$lang['lo']                    = 'weniger wichtig';
