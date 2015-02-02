6f3be2e59470c34b0b413148309bd89fb13f846f
rnck <dokuwiki@rnck.de>
2015-02-02 15:30:40 +0100

    translation update

--- lang/de/settings.php
+++ lang/de/settings.php
@@ -1,11 +1,14 @@
 <?php
-/**
- * German language file for config of Wrap plugin
- *
- */
 
-$lang['noPrefix'] = 'Welche (mit Komma getrennten) Klassen sollen nicht mit "wrap_" anfangen?';
-$lang['restrictedClasses'] = 'Beschränke die Benutzung des Plugins auf diese (mit Komma getrennten) Klassen';
-$lang['restrictionType'] = 'Beschränkungsart, setzt fest ob obige Klassen ein- oder ausgeschlossen werden sollen';
-  $lang['restrictionType_o_0'] = 'Erlaube alle Klassen außer die obigen';
-  $lang['restrictionType_o_1'] = 'Beschränke auf die obigen Klassen und erlaube keine anderen';
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author rnck <dokuwiki@rnck.de>
+ */
+$lang['noPrefix']              = 'Welche (mit Komma getrennten) Klassen sollen nicht mit "wrap_" anfangen?';
+$lang['restrictedClasses']     = 'Beschränke die Benutzung des Plugins auf diese (mit Komma getrennten) Klassen';
+$lang['restrictionType']       = 'Beschränkungsart, setzt fest ob obige Klassen ein- oder ausgeschlossen werden sollen';
+$lang['restrictionType_o_0']   = 'Erlaube alle Klassen außer die obigen';
+$lang['restrictionType_o_1']   = 'Beschränke auf die obigen Klassen und erlaube keine anderen';
+$lang['syntaxDiv']             = 'Welche Syntax soll in der Werkzeugleiste für Wrap Blöcke verwendet werden?';
+$lang['syntaxSpan']            = 'Welche Syntax soll in der Werkzeugleiste für inline Wraps verwendet werden?';
