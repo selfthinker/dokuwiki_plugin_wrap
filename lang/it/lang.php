0a46a0c53d74b86894bc9819b7e8c71a79e69914
Giovanni <Giovanni.Garofalo@outlook.com>
2015-03-25 19:25:19 +0100

    translation update

--- lang/it/lang.php
+++ lang/it/lang.php
@@ -4,8 +4,9 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Edmondo <snarchio@gmail.com>
+ * @author Giovanni <Giovanni.Garofalo@outlook.com>
  */
-$lang['column']                = 'colonna';
+$lang['column']                = 'colonne';
 $lang['em']                    = 'enfatizzato speciale';
 $lang['hi']                    = 'evidenziato';
 $lang['lo']                    = 'meno importante';
