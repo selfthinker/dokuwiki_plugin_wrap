b5ea7221eb42adb9dfde9ff725a623abf67dc6c9
Anika Henke <anika@selfthinker.org>
2015-05-04 13:05:15 +0100

    added missing German language strings

--- lang/de/settings.php
+++ lang/de/settings.php
@@ -9,3 +9,5 @@ $lang['restrictedClasses'] = 'Beschränke die Benutzung des Plugins auf diese (m
 $lang['restrictionType'] = 'Beschränkungsart, setzt fest ob obige Klassen ein- oder ausgeschlossen werden sollen';
   $lang['restrictionType_o_0'] = 'Erlaube alle Klassen außer die obigen';
   $lang['restrictionType_o_1'] = 'Beschränke auf die obigen Klassen und erlaube keine anderen';
+$lang['syntaxDiv'] = 'Welche Syntax soll die Werkzeugleiste für Block-Wraps verwenden?';
+$lang['syntaxSpan'] = 'Welche Syntax soll die Werkzeugleiste für Inline-Wraps verwenden?';
