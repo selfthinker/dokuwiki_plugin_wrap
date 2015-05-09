0babcac387c373057925606102928673f47b667c
DelD <deldadam@gmail.com>
2015-05-09 18:35:07 +0200

    translation update

--- lang/hu/settings.php
+++ lang/hu/settings.php
@@ -4,6 +4,7 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Marina Vladi <deldadam@gmail.com>
+ * @author DelD <deldadam@gmail.com>
  */
 $lang['noPrefix']              = 'Mely (vesszővel elválasztott) osztályneveknek ne legyen "wrap_" előtagja?';
 $lang['restrictedClasses']     = 'csatoló korlátozása ezekre a (vesszővel elválasztott) osztályokra';
@@ -11,4 +12,5 @@ $lang['restrictionType']       = 'korlátozás típusa, megadja, hogy a fenti os
 $lang['restrictionType_o_0']   = 'minden osztály engedélyezése, kivéve a fentieket';
 $lang['restrictionType_o_1']   = 'csak a fenti osztályok engedélyezése';
 $lang['syntaxDiv']             = 'Milyen szintaxist használjunk a blokktípusú dobozokhoz az eszközsorban?';
-$lang['syntaxSpan']            = 'Milyen szintaxist használjunk a sortípusú (inline) dobozokhoz az eszközsorban?';
+$lang['syntaxSpan']            = 'Milyen szintaxist használjunk a soron belüli (inline) dobozokhoz az eszközsorban?';
+$lang['darkTpl']               = 'Optimalizáljam a színeket sötét sablonokhoz?';
