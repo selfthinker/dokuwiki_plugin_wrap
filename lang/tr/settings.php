56b01c75502e2630320c3dac21a971194eb22ba6
İlker R. Kapaç <irifat@gmail.com>
2015-05-26 22:55:07 +0200

    translation update

--- lang/tr/settings.php
+++ lang/tr/settings.php
@@ -5,10 +5,11 @@
  * 
  * @author İlker R. Kapaç <irifat@gmail.com>
  */
-$lang['noPrefix']              = 'Hangi sınıf isimlerini, (virgülle ayrılmış) önüne "wrap_" öneki almaktan hariç tutulsun?';
+$lang['noPrefix']              = 'Hangi sınıf isimleri, (virgülle ayrılmış) önüne "wrap_" öneki almaktan hariç tutulsun?';
 $lang['restrictedClasses']     = 'eklentinin kullanımını bu sınıflarla (virgülle ayrılmış) sınırla';
 $lang['restrictionType']       = 'kısıtlama tipi, üstteki sınıfların dalil mi edileceklerini yoksa hariç mi tutulacaklarını belirler.';
 $lang['restrictionType_o_0']   = 'üsttekiler hariç tüm sınıflara izin ver';
 $lang['restrictionType_o_1']   = 'sadece üstteki sınıflarla sınırla, başkasına izin verme';
 $lang['syntaxDiv']             = 'Blok paketi için araç çubuğunda hangi sözdizimi kullanılsın?';
 $lang['syntaxSpan']            = 'Satır içi paketi için araç çubuğunda hangi sözdizimi kullanılsın?';
+$lang['darkTpl']               = 'Karanlık şablonlar için renkler iyileştirilsin mi?';
