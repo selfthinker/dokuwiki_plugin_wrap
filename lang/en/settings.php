024ed00c6d0f10eaf1aa6a26e981dfa50793a574
Aurora Voje <aurora.voje@jbv.no>
2015-07-13 15:45:35 +0200

    translation update

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,14 +1,18 @@
 <?php
-/**
- * English language file for config of Wrap plugin
- *
- */
 
-$lang['noPrefix'] = 'Which (comma separated) class names should be excluded from being prefixed with "wrap_"?';
-$lang['restrictedClasses'] = 'restrict usage of plugin to these (comma separated) classes';
-$lang['restrictionType'] = 'restriction type, specifies if classes above shall be included or excluded';
-  $lang['restrictionType_o_0'] = 'allow all classes except the ones above';
-  $lang['restrictionType_o_1'] = 'restrict to only the classes above and no others';
-$lang['syntaxDiv'] = 'Which syntax should be used in the toolbar picker for block wraps?';
-$lang['syntaxSpan'] = 'Which syntax should be used in the toolbar picker for inline wraps?';
-$lang['darkTpl'] = 'Optimise colours for dark templates?';
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Aurora Voje <aurora.voje@jbv.no>
+ */
+$lang['noPrefix']              = 'Hvilke klassenavn (adskilt med komma) bør ekskluderes fra å ha prefiksen "wrap_"?
+';
+$lang['restrictedClasses']     = 'Begrens bruken av plugin til klassene som er adskilt med komma.
+';
+$lang['restrictionType']       = 'restriksjonstype, spesifiserer om klassene ovenfor skal inkluderes eller ekskluderes
+';
+$lang['restrictionType_o_0']   = 'tillat alle klasser bortsett fra de ovenforstående';
+$lang['restrictionType_o_1']   = 'begrens kun til ovenforstående klasser og ingen fler';
+$lang['syntaxDiv']             = 'Hvilken syntaks bør brukes i verktøylinjen "velger" for "blokkomkransning"?';
+$lang['syntaxSpan']            = 'Hvilken syntaks bør brukes i verktøylinjen "velger" for omkransninger innenfor en linje?';
+$lang['darkTpl']               = 'Optimer farger for mørke maler?';
