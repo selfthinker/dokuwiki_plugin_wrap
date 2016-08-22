7d5a3edf8bdd7690bd42e93e50e9d7e86c562ab0
sam01 <m.sajad079@gmail.com>
2016-08-23 00:55:34 +0200

    translation update

--- lang/fa/lang.php
+++ lang/fa/lang.php
@@ -2,10 +2,12 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author reza_khn <reza_khn@yahoo.com>
+ * @author sam01 <m.sajad079@gmail.com>
  */
-$lang['column']                = 'ستون';
+$lang['picker']                = 'قرار دادن پلاگین';
+$lang['column']                = 'ستون‌ها';
 $lang['box']                   = 'کادر ساده';
 $lang['info']                  = 'کادر اطلاعات';
 $lang['tip']                   = 'کادر نکته';
@@ -16,5 +18,5 @@ $lang['download']              = 'کادر دانلود';
 $lang['todo']                  = 'کادر کاربردی';
 $lang['clear']                 = 'کادر شناور فعال';
 $lang['em']                    = 'تاکید';
-$lang['hi']                    = 'هایلایت';
+$lang['hi']                    = 'برجسته';
 $lang['lo']                    = 'کم اهمیت';
