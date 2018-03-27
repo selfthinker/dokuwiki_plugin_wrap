41d14527decfd0cff1bb61ef3b565f648f6d1c1e
Anika Henke <anika@selfthinker.org>
2018-03-27 22:48:08 +0100

    document use of wildcards in config settings

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -4,8 +4,8 @@
  *
  */
 
-$lang['noPrefix'] = 'Which (comma separated) class names should be excluded from being prefixed with "wrap_"?';
-$lang['restrictedClasses'] = 'restrict usage of plugin to these (comma separated) classes';
+$lang['noPrefix'] = 'Which (comma separated) class names should be excluded from being prefixed with "wrap_"? (* and ? wildcards allowed)';
+$lang['restrictedClasses'] = 'restrict usage of plugin to these (comma separated) classes (* and ? wildcards allowed)';
 $lang['restrictionType'] = 'restriction type, specifies if classes above shall be included or excluded';
   $lang['restrictionType_o_0'] = 'allow all classes except the ones above';
   $lang['restrictionType_o_1'] = 'restrict to only the classes above and no others';
