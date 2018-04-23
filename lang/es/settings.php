64b0897cf7f79f573f3ca4fdc8ce12262b7b146a
Domingo Redal <docxml@gmail.com>
2018-04-23 10:15:40 +0200

    translation update

--- lang/es/settings.php
+++ lang/es/settings.php
@@ -7,8 +7,8 @@
  * @author Juan Asensio Sánchez <okelet@gmail.com>
  * @author Óscar M. Lage <info@oscarmlage.com>
  */
-$lang['noPrefix']              = '¿Qué nombres de clase (separados por comas) no deberían ser precedidos de "wrap_"?';
-$lang['restrictedClasses']     = 'restringir el uso de este plugin a estas clases (separadas por comas)';
+$lang['noPrefix']              = '¿Qué nombres de clase (separados por comas) no deberían ser precedidos de "wrap_"? (caracteres comodines permitidos * e ?)';
+$lang['restrictedClasses']     = 'restringir el uso de este plugin a estas clases (separadas por comas) (caracteres comodines permitidos * e ?)';
 $lang['restrictionType']       = 'tipo de restricción, especifica si las clases anteriores serán incluidas o excluidas';
 $lang['restrictionType_o_0']   = 'permitir todas las clases excepto las anteriores';
 $lang['restrictionType_o_1']   = 'restringir a sólo las clases anteriores y no otras';
