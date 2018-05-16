8be843b094f629379686f27a847db5028a6a3e02
Oleksiy <alexey.furashev@gmail.com>
2018-05-16 15:35:26 +0200

    translation update

---
+++ lang/uk/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Oleksiy <alexey.furashev@gmail.com>
+ */
+$lang['restrictionType_o_0']   = 'Дозволити всі класи, крім вищезазначених';
+$lang['darkTpl']               = 'Оптимізувати кольори до темної гами оформлення?';
+$lang['emulatedHeadlines']     = 'Імітувати заголовки? (не підтримується)';
