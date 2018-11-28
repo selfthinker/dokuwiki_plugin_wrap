9a82f4cc371ec090126ade791a7f9738f2aa9342
Katrine Katapodi <katerinakatapodi@gmail.com>
2018-11-28 14:40:39 +0100

    translation update

---
+++ lang/el/lang.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Katrine Katapodi <katerinakatapodi@gmail.com>
+ */
+$lang['picker']                = 'Συμπεριλάβετε το επιρόσθετο';
+$lang['column']                = 'στήλες';
+$lang['box']                   = 'τετραγωνάκι';
+$lang['info']                  = 'κέντρο πληροφοριών';
+$lang['tip']                   = 'κέντρο εντολών';
+$lang['important']             = 'σημαντικό τετράγωνο';
+$lang['alert']                 = 'σύστημα συναγερμού';
+$lang['help']                  = 'σύστημα βοήθειας';
+$lang['download']              = 'σύστημα λήψης από τον υπολογιστή';
+$lang['todo']                  = 'σύστημα todo';
+$lang['em']                    = 'όπου έχει δοθεί ειδική έμφαση';
+$lang['hi']                    = 'με επισημάνσεις';
+$lang['lo']                    = 'λιγότερο σημαντικό';
