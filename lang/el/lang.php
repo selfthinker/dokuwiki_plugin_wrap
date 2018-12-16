e9ead1298a046ea88c4e6fce084d4890d0ab38fa
Katerina Katapodi <katerinakatapodi@gmail.com>
2018-12-16 18:05:38 +0100

    translation update

---
+++ lang/el/lang.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Katerina Katapodi <katerinakatapodi@gmail.com>
+ */
+$lang['picker']                = 'Wrap Plugin';
+$lang['column']                = 'στήλες';
+$lang['box']                   = 'απλό κέντρο πλαισίου';
+$lang['info']                  = 'πλαίσιο πληροφοριών ';
+$lang['tip']                   = 'πλαίσιο συμβουλών';
+$lang['important']             = 'σημαντικό πλαίσιο';
+$lang['alert']                 = 'πλαίσιο ειδοποιήσεων';
+$lang['help']                  = 'πλαίσιο ζήτησης βοήθειας';
+$lang['download']              = 'κατέβασμα πλαισίου';
+$lang['todo']                  = 'όλο το πλαίσιο';
+$lang['clear']                 = 'σβήστε τις αμφισημίες';
+$lang['em']                    = 'ειδική σημείωση';
+$lang['hi']                    = 'φωτισμένα ';
+$lang['lo']                    = 'λιγότερο σημαντικό ';
