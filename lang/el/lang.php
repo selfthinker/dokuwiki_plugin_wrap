74bd2448876d3f4d32a6da3a014eae8ee4ee497c
Aikaterini Katapodi <extragold1234@hotmail.com>
2019-07-18 19:40:13 +0200

    translation update

---
+++ lang/el/lang.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Aikaterini Katapodi <extragold1234@hotmail.com>
+ */
+$lang['picker']                = 'Wrap Plugin';
+$lang['column']                = 'στήλες';
+$lang['box']                   = 'κεντρικό κουτί';
+$lang['info']                  = 'χώρος πληροφοριών';
+$lang['tip']                   = 'χώρος συμβουλών';
+$lang['important']             = 'χώρος σημαντικών πληροφοριών ';
+$lang['alert']                 = 'χώρος προειδοποιήσεων ';
+$lang['help']                  = 'χώρος βοήθειας ';
+$lang['download']              = 'χώρος κατεβάσματος στον υπολογιστή';
+$lang['todo']                  = 'χώρος για όλα ';
+$lang['clear']                 = 'καθαρές υπενθυμίσεις';
+$lang['em']                    = 'με ειδική έμφαση';
+$lang['hi']                    = 'τονισμένο';
+$lang['lo']                    = 'λιγότερο σημαντικό ';
