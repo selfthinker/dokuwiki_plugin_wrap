f8f4ba2be26eb098bd1b202fad8b186265650591
Aikaterini Katapodi <extragold1234@hotmail.com>
2019-08-27 21:30:07 +0200

    translation update

---
+++ lang/el/lang.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Aikaterini Katapodi <extragold1234@hotmail.com>
+ */
+$lang['column']                = 'στήλες ';
+$lang['box']                   = 'απλό κουτί εστίασης';
+$lang['info']                  = 'κουτί πληροφοριών';
+$lang['tip']                   = 'κουτί συμβουλών ';
+$lang['important']             = 'σημαντικό κουτί';
+$lang['alert']                 = 'κουτί ενημέρωσης ';
+$lang['help']                  = 'κουτί παροχής βοήθειας';
+$lang['download']              = 'κατεβάστε το κιβώτιο στον υπολογιστή';
+$lang['todo']                  = 'όλο το κουτί';
+$lang['em']                    = 'με ιδιαίτερη έμφαση';
+$lang['hi']                    = 'φωτισμένο';
+$lang['lo']                    = 'λιγότερο σημαντικό';
