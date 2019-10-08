cf3e603780e1aa93b8669d6962cff7a8ddb1cd7c
Bartek S <sadupl@gmail.com>
2019-10-08 10:35:37 +0200

    translation update

---
+++ lang/pl/lang.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Bartek S <sadupl@gmail.com>
+ */
+$lang['column']                = 'kolumny';
+$lang['box']                   = 'proste wyśrodkowane pole';
+$lang['info']                  = 'pole informacji';
+$lang['important']             = 'ważne pole';
+$lang['alert']                 = 'pole ostrzeżenia';
+$lang['help']                  = 'pole pomocy';
+$lang['todo']                  = 'pole todo';
+$lang['hi']                    = 'podświetlony';
+$lang['lo']                    = 'mniej znaczące';
