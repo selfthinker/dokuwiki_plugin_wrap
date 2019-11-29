4ba85553b9cfa0c79e4c20f5f20e2c421abc9f8f
Petr Kajzar <petr.kajzar@lf1.cuni.cz>
2019-11-29 21:25:29 +0100

    translation update

--- lang/cs/lang.php
+++ lang/cs/lang.php
@@ -2,7 +2,8 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Petr Kajzar <petr.kajzar@lf1.cuni.cz>
  * @author Jaroslav Lichtblau <jlichtblau@seznam.cz>
  */
 $lang['picker']                = 'Zásuvný modul Wrap';
@@ -15,7 +16,7 @@ $lang['alert']                 = 'výstražný rámeček';
 $lang['help']                  = 'pomocný rámeček';
 $lang['download']              = 'rámeček s odkazem ke stažení';
 $lang['todo']                  = 'rámeček úkolu';
-$lang['clear']                 = 'clear floats';
+$lang['clear']                 = 'oddělovač plovoucích rámečků';
 $lang['em']                    = 'zvláštně zdůrazněné';
 $lang['hi']                    = 'zvýrazněné';
 $lang['lo']                    = 'méně důležité';
