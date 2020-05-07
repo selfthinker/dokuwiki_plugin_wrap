84a397178f6c5e06f9c8b6a41cb95073d81549a1
Thien Hau <thienhausoftware@gmail.com>
2020-05-07 04:30:37 +0200

    translation update

---
+++ lang/vi/settings.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Thien Hau <thienhausoftware@gmail.com>
+ */
+$lang['noPrefix']              = 'Những tên lớp (được phân tách bằng dấu phẩy) nên được loại trừ khỏi tiền tố với "wrap_"? ( * và ?)';
+$lang['restrictedClasses']     = 'Hạn chế sử dụng plugin đối với các lớp (được phân tách bằng dấu phẩy) (được phép ký tự đại diện * và ?)';
+$lang['restrictionType']       = 'Loại hạn chế, chỉ định nếu các lớp ở trên sẽ được bao gồm hoặc loại trừ';
+$lang['restrictionType_o_0']   = 'Cho phép tất cả các lớp trừ những lớp trên';
+$lang['restrictionType_o_1']   = 'Chỉ giới hạn các lớp ở trên và không có lớp nào khác';
+$lang['syntaxDiv']             = 'Cú pháp nào sẽ được sử dụng trong trình chọn thanh công cụ cho wrap khối?';
+$lang['syntaxSpan']            = 'Cú pháp nào nên được sử dụng trong trình chọn thanh công cụ cho wrap nội tuyến?';
+$lang['darkTpl']               = 'Tối ưu hóa màu sắc cho các chủ đề tối?';
+$lang['emulatedHeadlines']     = 'Sử dụng đầu đề mô phỏng? (lỗi thời)';
