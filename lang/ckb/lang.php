9068a72b6aa10a0ea1c0116b09cabea078420352
qezwan <qezwan@gmail.com>
2020-10-05 21:05:39 +0200

    translation update

---
+++ lang/ckb/lang.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author qezwan <qezwan@gmail.com>
+ */
+$lang['picker']                = 'پێوەکراو داپۆشە';
+$lang['column']                = 'ستوونەکان';
+$lang['box']                   = 'سندوقی ناوەندی سادە	';
+$lang['info']                  = 'سنووقی زانیاریی';
+$lang['tip']                   = 'سنووقی زانیاری		';
+$lang['important']             = 'سندوقی گرنگ';
+$lang['alert']                 = 'سندوقی ئاگادارکردنەوە		';
+$lang['help']                  = 'سندوقی یارمەتی';
+$lang['download']              = 'داگرتنی سنووق	';
+$lang['todo']                  = 'هەموو سندوقەکان		';
+$lang['clear']                 = 'سەرئاوکەوتن پاک			';
+$lang['em']                    = 'بەتایبەتی جەختی لەسەر			';
+$lang['hi']                    = 'هایلایت		';
+$lang['lo']                    = 'کەمتر بەرچاو';
