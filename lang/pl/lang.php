0d1b453a2d2b5d2dccdc8d5501c34486345dd013
Marek Adamski <fevbew@wp.pl>
2023-08-14 00:55:21 +0200

    translation update

--- lang/pl/lang.php
+++ lang/pl/lang.php
@@ -3,9 +3,10 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Marek Adamski <fevbew@wp.pl>
  * @author Bartek S <sadupl@gmail.com>
  */
-$lang['picker']                = 'Plugin Wrap';
+$lang['picker']                = 'Wtyczka Wrap';
 $lang['column']                = 'kolumny';
 $lang['box']                   = 'wyśrodkowane proste pole';
 $lang['info']                  = 'pole informacyjne';
@@ -15,5 +16,7 @@ $lang['alert']                 = 'pole ostrzeżenia';
 $lang['help']                  = 'pole pomocy';
 $lang['download']              = 'pole pobierania';
 $lang['todo']                  = 'pole todo';
+$lang['clear']                 = 'wyczyść elementy pływające';
+$lang['em']                    = 'szczególnie podkreślone';
 $lang['hi']                    = 'podświetlone';
 $lang['lo']                    = 'mniej znaczące';
