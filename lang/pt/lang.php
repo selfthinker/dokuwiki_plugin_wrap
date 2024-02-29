02fb5860e9fb78ae8cd419c3eac2ad0b19e9383e
Pedro Figueira <pffilosofia@hotmail.com>
2024-02-29 20:25:37 +0100

    translation update

---
+++ lang/pt/lang.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Pedro Figueira <pffilosofia@hotmail.com>
+ */
+$lang['picker']                = 'Extensão de encapsulamento';
+$lang['column']                = 'colunas';
+$lang['box']                   = 'simples caixa centralizada';
+$lang['info']                  = 'caixa de informações';
+$lang['tip']                   = 'caixa de dicas';
+$lang['important']             = 'caixa importante';
+$lang['alert']                 = 'caixa de alerta';
+$lang['help']                  = 'caixa de ajuda';
+$lang['download']              = 'caixa de transferência';
+$lang['todo']                  = 'caixa \'para fazer\'';
+$lang['clear']                 = 'limpar números racionais';
+$lang['em']                    = 'com ênfase especial';
+$lang['hi']                    = 'destacado';
+$lang['lo']                    = 'menos significante';
---
+++ lang/pt/settings.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Pedro Figueira <pffilosofia@hotmail.com>
+ */
+$lang['noPrefix']              = 'Quais nomes de classes (separados por vírgulas) devem ser excluídos de serem prefixados com "wrap_"? (* e ? caracteres coringa aceitos)';
+$lang['restrictedClasses']     = 'Restringir uso da extensão para essas classes (separadas por vírgula) (* e ? caracteres coringa aceitos)';
+$lang['restrictionType']       = 'Tipo de restrição, especifica se as classes acima devem ser incluídas ou excluídas';
+$lang['restrictionType_o_0']   = 'Permitir todas as classes, exceto as acima';
+$lang['restrictionType_o_1']   = 'Restringir para apenas as classes acima e nenhuma outra';
+$lang['syntaxDiv']             = 'Qual sintaxe deve ser usada no seletor da barra de ferramentas para encapsulamento de bloco?';
+$lang['syntaxSpan']            = 'Qual sintaxe deve ser usada no seletor da barra de ferramentas para encapsulamento em linha?';
+$lang['darkTpl']               = 'Otimizar cores para temas escuros?';
+$lang['emulatedHeadlines']     = 'Usar cabeçalhos emulados? (descontinuado)';
