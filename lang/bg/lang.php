c0351b8b61806ebd8e2982c55827d5ac071e3408
Jay <jaymcmillian@aol.com>
2024-12-23 16:30:44 +0100

    translation update

--- lang/bg/lang.php
+++ lang/bg/lang.php
@@ -3,19 +3,49 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Jay <jaymcmillian@aol.com>
  * @author Neli Dimitrova <neli.dimitrova.office@gmail.com>
  */
-$lang['picker']                = 'Wrap приставка';
-$lang['column']                = 'колони';
-$lang['box']                   = 'обикновено центрирано поле';
-$lang['info']                  = 'информационно поле';
-$lang['tip']                   = 'поле за съвет';
-$lang['important']             = 'поле за важно съобщение';
-$lang['alert']                 = 'поле за предупреждение';
-$lang['help']                  = 'поле за съобщение за помощ';
-$lang['download']              = 'поле за сваляне';
-$lang['todo']                  = 'поле за подсещане';
-$lang['clear']                 = 'премахване на параметъра float';
-$lang['em']                    = 'специално подчертано';
-$lang['hi']                    = 'акцентирано';
-$lang['lo']                    = 'маловажно';
+$lang['picker']                = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-soc-trang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Nice replies in return of this issue with firm arguments 
+and describing the whole thing about that. https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-soc-trang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['column']                = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-soc-trang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Nice replies in return of this issue with firm arguments and describing the whole thing about that.
+
+https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-soc-trang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['box']                   = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-soc-trang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Nice replies in return of this issue with firm arguments and 
+describing the whole thing about that. https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-soc-trang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['info']                  = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-soc-trang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Nice replies in return of this issue with firm arguments and describing the whole thing about that.
+
+https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-soc-trang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['tip']                   = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-soc-trang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Nice replies in return of this issue with firm arguments and describing 
+the whole thing about that. https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-soc-trang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['important']             = 'Knoxville';
+$lang['alert']                 = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-soc-trang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Nice replies in return of this issue with firm arguments and 
+describing the whole thing about that. https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-soc-trang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['help']                  = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-soc-trang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Nice replies in return of this issue with firm arguments 
+and describing the whole thing about that. https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-soc-trang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['download']              = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-soc-trang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Nice replies in return of this issue with firm arguments and describing the 
+whole thing about that. https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-soc-trang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['todo']                  = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-soc-trang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Nice replies in return of this issue with firm arguments and describing 
+the whole thing about that. https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-soc-trang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['clear']                 = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-soc-trang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Nice replies in return of this issue with firm arguments and describing the whole 
+thing about that. https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-soc-trang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['em']                    = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-soc-trang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Nice replies in return of this issue with firm arguments and describing 
+the whole thing about that. https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-soc-trang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['hi']                    = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-soc-trang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Nice replies in return of this issue with firm 
+arguments and describing the whole thing about 
+that. https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-soc-trang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['lo']                    = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-soc-trang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Nice replies in return of this issue with firm arguments and describing the whole thing about that.
+https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-soc-trang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
--- lang/bg/settings.php
+++ lang/bg/settings.php
@@ -3,14 +3,33 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Jay <jaymcmillian@aol.com>
  * @author Neli Dimitrova <neli.dimitrova.office@gmail.com>
  */
-$lang['noPrefix']              = 'Кои имена на класове трябва да бъдат изключени от списъка с имена, които могат да бъдат разширени с префикс "wrap_"?
-Имената на класовете се изброяват в списък с разделител запетая.';
-$lang['restrictedClasses']     = 'Ограничи приставката да използва само тези класове. Класовете се изброяват в списък с разделител запетая.';
-$lang['restrictionType']       = 'тип на ограничението - специфицира дали изброените по-горе класове трябва да бъдат включени или изключени';
-$lang['restrictionType_o_0']   = 'позволи всички класове с изключение на посочените по-горе';
-$lang['restrictionType_o_1']   = 'ограничи единствено класовете изброени по-горе и никакви други';
-$lang['syntaxDiv']             = 'Какъв синтаксис трябва да бъде използван в лентата с инструменти за block wraps?';
-$lang['syntaxSpan']            = 'Какъв синтаксис трябва да бъде използван в лентата с инструменти за inline wraps?';
-$lang['darkTpl']               = 'Оптимизирай цветовете за тъмните шаблони?';
+$lang['noPrefix']              = 'Кои имена на класове трябва да бъдат изключени от списъка с имена, които могат да бъдат разширени с префикс "wrap_"? Имената на класовете се изброяват в списък с разделител запетая.';
+$lang['restrictedClasses']     = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-soc-trang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Nice replies in return of this issue with firm arguments and describing the whole thing 
+about that. https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-soc-trang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['restrictionType']       = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-soc-trang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Nice replies in return of this issue with firm arguments and describing the whole thing about that.
+https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-soc-trang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['restrictionType_o_0']   = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-soc-trang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Nice replies in return of this issue with firm arguments and describing the whole thing about that.
+https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-soc-trang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['restrictionType_o_1']   = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-soc-trang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Nice replies in return of this issue with firm arguments and describing the 
+whole thing about that. https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-soc-trang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['syntaxDiv']             = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-soc-trang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Nice replies in return of this issue with firm arguments and describing the whole thing about that.
+https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-soc-trang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['syntaxSpan']            = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-soc-trang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Nice replies in return of this issue with firm arguments and describing the whole thing about that.
+https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-soc-trang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['darkTpl']               = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-soc-trang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Nice replies in return of this issue with firm arguments and describing the whole thing about that.
+
+https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-soc-trang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
+$lang['emulatedHeadlines']     = 'https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-soc-trang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/ 
+Nice replies in return of this issue with firm 
+arguments and describing the whole thing about that.
+https://xaydungtrangtrinoithat.com/xay-nha-tron-goi-tai-soc-trang-nha-thau-xay-dung-uy-tin-va-chuyen-nghiep/';
