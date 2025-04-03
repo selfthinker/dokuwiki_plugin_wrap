f0b62e91abceb3cae918b7d3c845ac31e684464e
Tiberiu Popescu <popescu.tibi@gmail.com>
2025-04-03 16:55:44 +0200

    translation update

---
+++ lang/ro/settings.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Tiberiu Popescu <popescu.tibi@gmail.com>
+ */
+$lang['noPrefix']              = 'Ce clase (separate cu virgulă) vor fi excluse de la prefixarea cu "wrap_"? (caracterele speciale * și ? sunt permise)';
+$lang['restrictedClasses']     = 'Restricționează utilizarea plugin-ului la aceste clase (separate cu virgulă, caracterele speciale * și ? sunt permise)';
+$lang['restrictionType']       = 'Tip de restricție, specifică dacă clasele de mai sus vor fi incluse sau excluse';
+$lang['restrictionType_o_0']   = 'Permite toate clasele cu excepția celor de mai sus';
+$lang['restrictionType_o_1']   = 'Permite numai clasele de mai sus';
+$lang['syntaxDiv']             = 'Ce sintaxe pentru blocuri apar în lista de unelte?';
+$lang['syntaxSpan']            = 'Ce sintaxe pentru liniile de text apar în lista de unelte?';
+$lang['darkTpl']               = 'Optimizez culorile pentru teme întunecate?';
