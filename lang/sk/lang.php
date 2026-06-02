5af9a8f21d801a591fcb20209e49463ea5838176
Wizzard <wizzardsk@gmail.com>
2026-06-02 20:50:45 +0200

    Translation update (sk)

--- lang/sk/lang.php
+++ lang/sk/lang.php
@@ -2,11 +2,21 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Wizzard <wizzardsk@gmail.com>
  * @author Martin Michalek <michalek.dev@gmail.com>
  */
 $lang['picker']                = 'Wrap Plugin';
 $lang['column']                = 'stĺpec';
+$lang['box']                   = 'jednoduchý vycentrovaný box';
+$lang['info']                  = 'informačný box';
+$lang['tip']                   = 'box s tipom';
+$lang['important']             = 'dôležitý box';
+$lang['alert']                 = 'výstražný box';
+$lang['help']                  = 'box s nápovedou';
+$lang['download']              = 'box na stiahnutie';
+$lang['todo']                  = 'úlohový box';
+$lang['clear']                 = 'zrušiť obtekanie';
 $lang['em']                    = 'zvlášť zdôraznený';
 $lang['hi']                    = 'zvýraznený';
 $lang['lo']                    = 'menej významný';
--- lang/sk/settings.php
+++ lang/sk/settings.php
@@ -2,7 +2,8 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Wizzard <wizzardsk@gmail.com>
  * @author Martin Michalek <michalek.dev@gmail.com>
  */
 $lang['noPrefix']              = 'Ktoré (čiarkou oddelené) mená tried by mali byť vynechané pri použití predpony "wrap_"?';
@@ -10,3 +11,7 @@ $lang['restrictedClasses']     = 'Obmedzenie použitia pluginu na tieto (čiarko
 $lang['restrictionType']       = 'Typ obmedzenia, špecifikuje, či triedy uvedené vyššie maju byť zahrnuté alebo vynechané';
 $lang['restrictionType_o_0']   = 'povolenie pre všetky triedy okrem uvedených vyššie';
 $lang['restrictionType_o_1']   = 'obmedzenie len na triedy uvedené vyššie a žiadne iné';
+$lang['syntaxDiv']             = 'Aká syntax sa má použiť v paneli nástrojov pre blokové obaly (block wraps)?';
+$lang['syntaxSpan']            = 'Aká syntax sa má použiť v paneli nástrojov pre riadkové obaly (inline wraps)?';
+$lang['darkTpl']               = 'Optimalizovať farby pre tmavé šablóny?';
+$lang['emulatedHeadlines']     = 'Použiť emulované nadpisy? (zastarané)';
